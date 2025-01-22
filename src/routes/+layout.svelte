4662218b59b96f43070f5b3c663fbebb6995cdfb
Arash <ghalbeyou6@gmail.com>
2025-01-22 17:06:49 +0330

    A bit of marketing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+    <title>Fetch GitHub Repository Information for free - GitCalc</title> 
+</svelte:head>
+
 <div class="relative flex min-h-screen items-center justify-center bg-gray-100 p-4">
 	{@render children()}
 </div>
